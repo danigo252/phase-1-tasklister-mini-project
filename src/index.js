ff13316c488edc1d3d9d3c242acc54c41fb5bf43
daniel <danielgonzalez252@gmail.com>
2023-04-16 22:32:09 -0400

    Lab Created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+    document.addEventListener("DOMContentLoaded", () => {
+        const taskForm = document.getElementById("create-task-form");
+        const taskList = document.getElementById("tasks");
+
+        taskForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const taskInput = document.getElementById("new-task-description");
+            const newTask = document.createElement("li");
+            newTask.innerText = taskInput.value;
+            taskList.appendChild(newTask);
+            taskInput.value = "";
+        });
+    });
+
+
